d1cc419516bd65c82f1f9a990c9f7d07e564bf82
lolohole <basemhalaika@gmail.com>
2025-07-16 13:43:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 });
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // إعداد EJS
