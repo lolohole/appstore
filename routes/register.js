288f40a11d9c6162038994ebae11a4a7a6b54c96
lolohole <basemhalaika@gmail.com>
2025-07-17 13:00:05 +0300

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -1,14 +1,18 @@
-const express = require('express'); // ← تأكد من إضافته هنا
+const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-router.post('/register', async (req, res) => {
+
+router.get('/', (req, res) => {
+  res.render('register');
+});
+
+router.post('/', async (req, res) => {
   try {
     const { name, email, tiktokUsername } = req.body;
 
     const newUser = new User({ name, email, tiktokUsername });
     await newUser.save();
 
-    // حفظ الجلسة
     req.session.user = {
       id: newUser._id,
       name: newUser.name,
@@ -23,4 +27,4 @@ router.post('/register', async (req, res) => {
   }
 });
 
-module.exports = router; 
+module.exports = router;
