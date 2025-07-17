a444f001b8cbf6015403003e594cf2bc09d63287
lolohole <basemhalaika@gmail.com>
2025-07-17 12:59:26 +0300

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -3,26 +3,22 @@ const router = express.Router();
 const User = require('../models/User'); // تأكد أن المسار صحيح
 
 // صفحة تسجيل الدخول - GET
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
   res.render('login');
 });
 
 // تسجيل الدخول - POST
-router.post('/login', async (req, res) => {
+router.post('/', async (req, res) => {
   const { email } = req.body;
 
   try {
-    // 🔍 البحث عن المستخدم
     const foundUser = await User.findOne({ email });
 
     if (!foundUser) {
       return res.send('البريد الإلكتروني غير مسجل.');
     }
 
-    // حفظ المستخدم في الجلسة
     req.session.user = foundUser;
-
-    // إعادة التوجيه إلى الصفحة الرئيسية
     res.redirect('/');
   } catch (error) {
     console.error(error);
