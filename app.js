b9b0ad17ef824e2607bc7a54292080eb21d5a211
lolohole <basemhalaika@gmail.com>
2025-07-17 03:16:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -129,6 +129,4 @@ function detectDevice(ua) {
 // ✅ تشغيل السيرفر
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
-});
+module.exports = app;
